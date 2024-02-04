bcfbaba241c654e97bc181e18538eebb3c600743
SANGEKARI SHIVA NAGARAJU <122778072+Shivasangekari@users.noreply.github.com>
2024-02-04 23:01:12 +0530

    Update Lecture13.java

--- Lecture13.java
+++ Lecture13.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+public class Lecture13 {
+    int n1, n2, result;
+    Scanner sc =new Scanner(System.in);
+    void calculate() {
+        try {
+            System.out.println("Enter First Number "); 
+            n1=sc.nextInt();
+            System.out.println("Enter Second Number"); 
+            n2=sc.nextInt();
+            
+            result = n1/n2;
+            
+        }
+        catch(Exception e) {
+            System.out.println("Ouch! Exception Detected!");
+            
+        }
+        System.out.println("The Division is "+result);
+        
+    }
+    public static void main(String[] args) {
+        Lecture13 obj = new Lecture13();
+        obj.calculate();
+        
+    }
+    
+}
