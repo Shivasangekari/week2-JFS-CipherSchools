e0e6534dd76169dd4d0306f159a91a9f9b3d2a14
SANGEKARI SHIVA NAGARAJU <122778072+Shivasangekari@users.noreply.github.com>
2024-02-05 00:13:48 +0530

    Update Lecture12.java

--- Lecture12.java
+++ Lecture12.java
@@ -1,1 +1,27 @@
+public class Lecture12 {
 
+// this keyword
+
+int arg=5;
+
+Lecture12()
+{
+    System.out.println("Hi ! i am default constructor");
+}
+Lecture12(int arg) {
+this();
+this.arg=arg;
+
+}
+
+public static void main(String[] args) {
+
+int arg=10;
+
+Lecture12 obj = new Lecture12(10);
+
+System.out.println(obj.arg);
+
+}
+
+}
