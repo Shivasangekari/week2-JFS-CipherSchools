bd1adcaf636fdbf6def3bc75736366d5ccef8029
SANGEKARI SHIVA NAGARAJU <122778072+Shivasangekari@users.noreply.github.com>
2024-02-04 23:53:03 +0530

    Update Lecture16.java

--- Lecture16.java
+++ Lecture16.java
@@ -1,1 +1,27 @@
+import java.util.Iterator;
 
+import java.util.Vector;
+
+public class Lecture16 {
+
+public static void main(String[] args) {
+
+Vector vec = new Vector();
+
+vec.add("texas");
+
+vec.add("zurich");
+
+vec.add("newyork");
+
+Iterator pointer = vec.iterator();
+
+while(pointer.hasNext()) {
+
+System.out.println(pointer.next());
+
+}
+
+}
+
+}
