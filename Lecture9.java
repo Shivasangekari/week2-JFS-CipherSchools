03bc20abbf67b3b94b8622fbc7693644b55fd60b
SANGEKARI SHIVA NAGARAJU <122778072+Shivasangekari@users.noreply.github.com>
2024-02-04 21:11:15 +0530

    Update Lecture9.java

--- Lecture9.java
+++ Lecture9.java
@@ -1,1 +1,32 @@
+public class Lecture9 {
 
+int width;
+int height;
+
+void display(String item) {
+    System.out.println(item +" Width "+width+" Height "+height);
+}
+
+public static void main(String[] args) {
+    Chair shiva = new Chair();
+    shiva.foo();
+    }
+}
+
+class Chair extends Lecture9{
+    void foo()
+    {width=10;height=20; display("Chair");}
+
+}
+
+class Table extends Lecture9{ 
+    void foo()
+    {width=30;height=50; display("Table");}
+
+}
+
+class Almira extends Lecture9{
+    void foo() 
+    {width=100;height=200;display("Almira");}
+
+}
