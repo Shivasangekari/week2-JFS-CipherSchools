8a816ad169b1e33fd26cb2ecf784fc6e2bd5599b
SANGEKARI SHIVA NAGARAJU <122778072+Shivasangekari@users.noreply.github.com>
2024-02-04 21:46:13 +0530

    Update Lecture10.java

--- Lecture10.java
+++ Lecture10.java
@@ -1,1 +1,25 @@
+//overloading
+public class Lecture10 {
+    
+void add(int num1, int num2)
+ {System.out.println(num1+num2);}
 
+void add(int num1, int num2, int num3)
+ {System.out.println(num1+num2+num3);}
+
+void add(float num1, float num2)
+ {System.out.println(num1+num2);}
+
+void add(int num1, float num2)
+ {System.out.println(num1+num2);}
+
+void add(float num1, int num2)
+ {System.out.println(num1+num2);}
+
+public static void main(String[] args) {
+    Lecture10 l = new Lecture10(); 
+    l.add(12.2f, 300);
+    
+}
+
+}
